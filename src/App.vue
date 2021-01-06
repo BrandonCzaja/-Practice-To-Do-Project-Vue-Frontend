87f814d623450fdf9280b042a99d432aba152e9c
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-06 10:09:43 -0600

    Can edit and delete To Dos

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,20 @@
       <input type="submit" />
     </form>
     <hr />
+    <h1>Edit a To Do</h1>
+    <form v-on:submit.prevent="editTodo">
+      <input type="text" v-model="editTitle" />
+      <input type="text" v-model="editBody" />
+      <input type="submit" />
+    </form>
     <h3>To Dos</h3>
     <ul>
       <li v-for="todo of todos" v-bind:key="todo.id">
         **********************
         <h4>{{todo.title}}</h4>
         <h5>{{todo.body}}</h5>**********************
+        <button v-on:click="()=> editSelect(todo)">Edit</button>
+        <button v-on:click="()=> deleteTodo(todo)">Delete</button>
       </li>
     </ul>
   </div>
@@ -30,7 +38,10 @@ export default {
       todos: [],
       baseUrl: "http://localhost:3000/todos",
       createTitle: "",
-      createBody: ""
+      createBody: "",
+      editTitle: "",
+      editBody: "",
+      editId: 0
     };
   },
   // methods is an object of functions
@@ -51,7 +62,36 @@ export default {
           body: this.createBody
         })
       });
-      (this.createTitle = ""), (this.createBody = ""), this.getTodos();
+      this.createTitle = "";
+      this.createBody = "";
+      this.getTodos();
+    },
+    editSelect: function(todo) {
+      this.editTitle = todo.title;
+      this.editBody = todo.body;
+      this.editId = todo.id;
+    },
+    editTodo: async function() {
+      await fetch(this.baseUrl + "/" + this.editId, {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title: this.editTitle,
+          body: this.editBody
+        })
+      });
+      this.editTitle = "";
+      this.editBody = "";
+      this.editId = 0;
+      this.getTodos();
+    },
+    deleteTodo: async function(todo) {
+      await fetch(this.baseUrl + "/" + todo.id, {
+        method: "delete"
+      });
+      this.getTodos();
     }
   },
   // create runs after components is initially created, one of many lifecycle functions
